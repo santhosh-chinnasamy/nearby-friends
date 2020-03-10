e97e341ad924fe921a14f24a3d8725a5e767e050
Santhosh-Chinnasamy <csesanthosh15@gmail.com>
2020-03-10 21:55:14 +0530

    change routes

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,15 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user');
 const auth = require('../utils/authentication');
-
-/* GET home page. */
+const validator = require('../utils/validation');
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Nearby Friends API' });
 });
 
-router.post('/login', userController.login);
-router.post('/signup', userController.signup);
+router.post('/login', validator.validate_login, userController.login);
+router.post('/signup', validator.validate_signup, userController.signup);
 router.post('/import_csv', userController.import_csv);
-router.post('/friends', auth, userController.users_nearme);
-router.post('/view_profile', auth, userController.profile);
+router.post('/nearby', auth, validator.validate_nearby, userController.users_nearby);
+router.post('/view_profile/:id', userController.profile);
 router.post('/edit_profile', auth, userController.edit_profile);
 module.exports = router;
